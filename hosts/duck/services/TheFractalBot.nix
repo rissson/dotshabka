1c79d377517d1c70d7b92cb724b0775106f8c472
ddorn <diego.dorn@free.fr>
2019-12-30 10:24:25 +0100

    hosts: duck: services: change TheFractalBot start time

--- hosts/duck/services/TheFractalBot.nix
+++ hosts/duck/services/TheFractalBot.nix
@@ -4,7 +4,7 @@
   systemd.services.TheFractalBot = {
     description = "FIXME";
     script = "exec ${pkgs.nur.repos.risson.brocoli}/bin/brocoli bot";
-    startAt = "09:09";
+    startAt = "07:42";
 
     serviceConfig.User = "diego";
     serviceConfig.Group = "mine";
