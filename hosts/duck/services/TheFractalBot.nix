f65186b15e9cfc88811f1d0fd36a099445197848
ddorn <diego.dorn@free.fr>
2019-12-31 11:26:47 +0100

    hosts: duck: services: update TheFractalBot description

--- hosts/duck/services/TheFractalBot.nix
+++ hosts/duck/services/TheFractalBot.nix
@@ -2,7 +2,7 @@
 
 {
   systemd.services.TheFractalBot = {
-    description = "FIXME";
+    description = "Brocoli, a bot that posts a new random fractal everyday on Twitter.";
     script = "exec ${pkgs.nur.repos.risson.brocoli}/bin/brocoli bot";
     startAt = "07:42";
 
