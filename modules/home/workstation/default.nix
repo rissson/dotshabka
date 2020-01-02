243f605a21a7ca6788a4e2e6863fc7e44091c850
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2020-01-02 21:44:54 +0100

    modules: home: workstation: enable random-background by default

--- modules/home/workstation/default.nix
+++ modules/home/workstation/default.nix
@@ -6,10 +6,16 @@ with import <shabka/util>;
 
 {
   config = mkIf config.shabka.workstation.enable {
-    home = {
-      packages = with pkgs; [
-        thunderbird
-      ];
+    home.packages = with pkgs; [
+      thunderbird
+    ];
+
+    services.random-background = {
+      enable = true;
+      enableXinerama = true;
+      display = "center";
+      imageDirectory = "%h/.background-images";
+      interval = "1h";
     };
   };
 }
