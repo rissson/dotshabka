3a36b2f7c01fabceb9efee92e6a7e3db24dfda2f
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2020-01-28 10:13:03 +0100

    hosts: duck: services: add hercules-ci

---
+++ hosts/duck/services/hercules-ci.nix
@@ -0,0 +1,16 @@
+{ config, pkgs, lib, ... }:
+
+with lib;
+
+{
+  imports = [
+    ( builtins.fetchTarball "https://github.com/hercules-ci/hercules-ci-agent/archive/stable.tar.gz"
+      + "/module.nix"
+    )
+  ];
+
+  services.hercules-ci-agent = {
+    enable = true;
+    concurrentTasks = 4;
+  };
+}
