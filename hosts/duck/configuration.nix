74fdbbbd14e8ccb134ad79d038d0a01229ce4770
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2020-02-09 23:08:21 +0100

    hosts: duck: deploy user: add rsync as package

--- hosts/duck/configuration.nix
+++ hosts/duck/configuration.nix
@@ -89,6 +89,8 @@ in {
       home = "/srv";
       isSystemUser = true;
       group = "deploy";
+      packages = with pkgs; [ rsync ];
+      shell = pkgs.bash;
       openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEfjcrIG01Wh266+cEL3ib80dJkyYxoMVFUaxQch1xnv" ];
     };
   };
