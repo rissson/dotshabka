558e7d4504eea7435d0466d3212fa012b5c914d6
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2020-02-09 22:09:31 +0000

    hosts: duck: add user for GitLab CI automated deployments

--- hosts/duck/configuration.nix
+++ hosts/duck/configuration.nix
@@ -84,6 +84,21 @@ in {
     };
   };
 
+  users.extraUsers = {
+    "gitlabci" = {
+      home = "/srv";
+      isSystemUser = true;
+      group = "deploy";
+      packages = with pkgs; [ rsync ];
+      shell = pkgs.bash;
+      openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEfjcrIG01Wh266+cEL3ib80dJkyYxoMVFUaxQch1xnv" ];
+    };
+  };
+
+  users.extraGroups = {
+    "deploy" = {};
+  };
+
   shabka.virtualisation = {
     docker.enable = true;
     libvirtd.enable = true;
