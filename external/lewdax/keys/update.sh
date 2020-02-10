12992231446393ce75ab64f13c2141eb95136c44
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2020-02-10 01:39:14 +0100

    external: lewdax: update ssh keys

--- external/lewdax/keys/update.sh
+++ external/lewdax/keys/update.sh
@@ -5,4 +5,4 @@ set -euo pipefail
 readonly here="$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)"
 readonly shabka_path="$(cd "${here}"/../../../ && pwd)"
 
-echo "{\"url\":\"https://github.com/animaxwell.keys\",\"sha256\":\"$(nix-prefetch-url https://github.com/animaxwell.keys)\"}" > "${here}/version.json"
+echo "{\"url\":\"https://gitlab.com/hanewaifu.keys\",\"sha256\":\"$(nix-prefetch-url https://gitlab.com/hanewaifu.keys)\"}" > "${here}/version.json"
