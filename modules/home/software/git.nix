f95ec9d5031c6a76e2d5810585dbf9197353ca0b
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2020-03-04 20:29:19 +0100

    modules: home: software: git: change gpgkey for CRI repositories

--- modules/home/software/git.nix
+++ modules/home/software/git.nix
@@ -3,38 +3,47 @@
 with lib;
 
 {
-  config = {
+  shabka.git = {
+    userName = "Marc 'risson' Schmitt";
+    userEmail = "marc.schmitt@risson.space";
+    gpgSigningKey = "marc.schmitt@risson.space";
+  };
 
-    shabka.git = {
-      userName = "Marc 'risson' Schmitt";
-      userEmail = "marc.schmitt@risson.space";
-      gpgSigningKey = "marc.schmitt@risson.space";
+  programs.git.package = mkForce pkgs.gitAndTools.gitFull;
+  programs.git.aliases = {
+    b         = "branch";
+    coke      = "commit -a -m";
+    cokewogpg = "commit --no-gpg-sign -a -m";
+  };
+
+  programs.git.extraConfig = {
+
+    branch = {
+      autosetuprebase = "always";
     };
 
-    programs.git.package = mkForce pkgs.gitAndTools.gitFull;
-    programs.git.aliases = {
-      b         = "branch";
-      coke      = "commit -a -m";
-      cokewogpg = "commit --no-gpg-sign -a -m";
+    core = {
+      editor = "vim";
     };
 
-    programs.git.extraConfig = {
+    push = {
+      default = "simple";
+    };
 
-      branch = {
-        autosetuprebase = "always";
-      };
-
-      core = {
-        editor = "vim";
-      };
-
-      push = {
-        default = "simple";
-      };
-
-      pull = {
-        rebase = true;
-      };
+    pull = {
+      rebase = true;
     };
   };
+
+  includes = [
+    {
+      condition = "gitdir:~/cri/";
+      contents = {
+        user = {
+          email = "risson@cri.epita.fr";
+          signingkey = "risson@cri.epita.fr";
+        };
+      };
+    }
+  ];
 }
