97667118c69c97caeb1b6bea84cebfae62ec8528
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2020-04-02 15:40:02 +0200

    hosts: add nas

---
+++ hosts/nas/configuration.nix
@@ -0,0 +1,114 @@
+{ pkgs, lib, ... }:
+
+with lib;
+
+let
+  shabka = import <shabka> { };
+
+  dotshabka = import ../.. { };
+
+  haveSecrets = builtins.pathExists ./../../secrets;
+in {
+  imports =
+    [
+      <shabka/modules/nixos>
+
+      ../../modules/nixos
+
+      ./hardware-configuration.nix
+
+      ./networking.nix
+
+      ./home.nix
+    ]
+    ++ (optionals haveSecrets (singleton ./../../secrets));
+
+  services.netdata = {
+    enable = true;
+  };
+
+  services.smartd = {
+    enable = true;
+    extraOptions = [
+      "-A /var/log/smartd/"
+      "--interval=600"
+    ];
+    devices = [
+      { device = "/dev/sda"; }
+      { device = "/dev/sdb"; }
+      { device = "/dev/sdc"; }
+    ];
+  };
+
+  services.logrotate = {
+    enable = true;
+    config = ''
+      compress
+      /var/log/smartd/* {
+        rotate 5
+        weekly
+        olddir /var/log/smartd/old
+      }
+    '';
+  };
+
+  i18n.defaultLocale = "en_US.UTF-8";
+  time.timeZone = "Europe/Paris";
+  shabka.keyboard.layouts = [ "qwerty" ];
+  shabka.keyboard.enableAtBoot = true;
+
+  services.openssh = {
+    enable = true;
+    passwordAuthentication = false;
+    permitRootLogin = "no";
+    hostKeys = [
+      {
+        type = "rsa";
+        bits = 4096;
+        path = "/etc/ssh/ssh_host_rsa_key";
+        rounds = 100;
+        openSSHFormat = true;
+        comment = "nas.barr.srv.lama-corp.space";
+      }
+      {
+        type = "ed25519";
+        path = "/etc/ssh/ssh_host_ed25519_key";
+        rounds = 100;
+        openSSHFormat = true;
+        comment = "nas.barr.srv.lama-corp.space";
+      }
+    ];
+  };
+
+  users.users.root.openssh.authorizedKeys.keys = (singleton dotshabka.external.risson.keys)
+    ++ (singleton dotshabka.external.diego.keys);
+  shabka.users.enable = true;
+  shabka.users.users = {
+    risson = {
+      uid = 2000;
+      isAdmin = true;
+      home ="/home/risson";
+      hashedPassword = "$6$2YnxY3Tl$kRj7YZypnB2Od41GgpwYRcn4kCcCE6OksZlKLws0rEi//T/emKWEsUZZ2ZG40eph1bpmjznztav4iKc8scmqc1";
+      sshKeys = singleton dotshabka.external.risson.keys;
+    };
+    diego = {
+      uid = 2005;
+      isAdmin = true;
+      home = "/home/diego";
+      hashedPassword = "$6$QMhH.GTGHaI3FgjF$DFKr7yQujSyv2bPjgVdWGmqwgP5ArGmoBcAR9E9P/f9JTD2PRUtRGhOKymyWswB.Dh4JW9Vd4JZ.wz0iOOIPS/";
+      sshKeys = singleton dotshabka.external.diego.keys;
+    };
+  };
+
+  shabka.virtualisation = {
+    docker.enable = true;
+  };
+
+  shabka.neovim.enable = true;
+
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  system.stateVersion = "19.09"; # Did you read the comment?
+}
---
+++ hosts/nas/networking.nix
@@ -0,0 +1,46 @@
+{ config, pkgs, lib, ... }:
+
+with lib;
+
+{
+  networking.hostName = "nas"; # Define your hostname.
+  networking.domain = "srv.lama-corp.space";
+  networking.hostId = "3474d85a";
+
+  networking.nameservers = [
+    "1.1.1.1" "1.0.0.1" "208.67.222.222"
+    "2606:4700:4700::1111" "2606:4700:4700::1001" "2620:119:35::35"
+  ];
+
+  networking.useDHCP = false;
+  networking.interfaces."bond0" = {
+    ipv4.addresses = [
+      { address = "192.168.44.253"; prefixLength = 24; }
+    ];
+  };
+
+  networking.defaultGateway = {
+    address = "192.168.44.254";
+    interface = "bond0";
+  };
+
+  networking.bonds."bond0" = {
+    interfaces = [ "enp3s0" "enp4s0" ];
+    driverOptions = {
+      mode = "balance-alb";
+    };
+  };
+
+  networking.firewall = {
+    enable = true;
+    allowPing = true;
+
+    allowedTCPPorts = [ 22 19999 ];
+    allowedUDPPorts = [ ] ++ (optionals config.networking.wireguard.enable (singleton 51820)); # Wireguard
+
+    allowedTCPPortRanges = [ ];
+    allowedUDPPortRanges = [
+      { from = 60000; to = 61000; } # mosh
+    ];
+  };
+}
