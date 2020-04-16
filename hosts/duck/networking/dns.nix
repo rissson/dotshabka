c1548a1e2bf485ee89dbd27565f27f0e26f7143a
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2020-04-16 21:07:14 +0200

    add dns base config

--- hosts/duck/networking/dns.nix
+++ hosts/duck/networking/dns.nix
@@ -1,5 +1,20 @@
 { ... }:
 
-{
-
+let
+  dotshabka = import <dotshabka> {};
+in {
+  services.unbound = {
+    enable = true;
+    allowedAccess = [ "172.28.0.0/16" ];
+    interfaces = [ "127.0.0.1" "::1" "172.28.1.1" ];
+    extraConfig = ''
+      forward-zone:
+        name: "."
+        forward-tls-upstream: yes
+        forward-addr: 45.90.28.0#61ba3a.dns1.nextdns.io
+        forward-addr: 2a07:a8c0::#61ba3a.dns1.nextdns.io
+        forward-addr: 45.90.30.0#61ba3a.dns2.nextdns.io
+        forward-addr: 2a07:a8c1::#61ba3a.dns2.nextdns.io
+    '';
+  };
 }
