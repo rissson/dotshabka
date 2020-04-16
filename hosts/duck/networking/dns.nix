dcb566c6824e9ffb198747fb87fcd95b321442c5
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2020-04-16 22:53:47 +0200

    dns: resolve private hosts

--- hosts/duck/networking/dns.nix
+++ hosts/duck/networking/dns.nix
@@ -11,6 +11,7 @@ in with dotshabka.data.iPs; {
     allowedAccess = [ "0.0.0.0/0" "::0/0" ];
     enableRootTrustAnchor = true;
     extraConfig = ''
+        tls-cert-bundle: ${pkgs.cacert}/etc/ssl/certs/ca-certificates.crt
         # Do not query the following addresses. No DNS queries are sent there.
         # List one address per entry. List classless netblocks with /size,
         # do-not-query-address: 127.0.0.1/8
@@ -34,10 +35,23 @@ in with dotshabka.data.iPs; {
         insecure-lan-zones: yes
 
         domain-insecure: "bar.lama-corp.space"
-        domain-insecure: "fly.lama-corp.space"
-        domain-insecure: "fsn.lama-corp.space"
-
         local-zone: "44.168.192.in-addr.arpa." transparent
+        local-data: "nas.srv.bar.lama-corp.space. IN A 172.28.2.1"
+        local-data-ptr: "172.28.2.1 nas.srv.bar.lama-corp.space"
+
+        domain-insecure: "fly.lama-corp.space"
+        local-zone: "fly.lama-corp.space." static
+        local-data: "hedgehog.lap.fly.lama-corp.space. IN A 172.28.101.1"
+        local-data-ptr: "172.28.101.1 hedgehog.lap.fly.lama-corp.space"
+        local-data: "trunck.lap.fly.lama-corp.space. IN A 172.28.102.1"
+        local-data-ptr: "172.28.102.1 "trunck.lap.fly.lama-corp.space"
+
+        domain-insecure: "fsn.lama-corp.space"
+        local-zone: "fsn.lama-corp.space." static
+        local-data: "duck.srv.fsn.lama-corp.space. IN A 172.28.1.1"
+        local-data-ptr: "172.28.1.1 "duck.srv.fsn.lama-corp.space"
+        local-data: "hub.virt.duck.fsn.lama-corp.space. IN A 172.28.1.11"
+        local-data-ptr: "172.28.1.11 "hub.virt.duck.fsn.lama-corp.space"
 
       forward-zone:
         name: "bar.lama-corp.space."
@@ -53,6 +67,8 @@ in with dotshabka.data.iPs; {
         forward-addr: 2a07:a8c0::#61ba3a.dns1.nextdns.io
         forward-addr: 45.90.30.0#61ba3a.dns2.nextdns.io
         forward-addr: 2a07:a8c1::#61ba3a.dns2.nextdns.io
+        forward-addr: 1.1.1.1@853#cloudflare-dns.com
+        forward-addr: 1.0.0.1@853#cloudflare-dns.com
     '';
   };
 }
