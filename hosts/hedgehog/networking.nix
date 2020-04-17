944bbb10c932e7e2dba8c2113fbec7ed2a390691
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2020-04-17 22:17:22 +0200

    hosts: hedgehog: networking: forward bar's network through VPN

--- hosts/hedgehog/networking.nix
+++ hosts/hedgehog/networking.nix
@@ -44,7 +44,7 @@ in {
         peers = [
           { # duck.srv.fsn.lama-corp.space
             publicKey = "CCA8bRHyKy7Er430MPwrNPS+PgLelCDKsaTos/Z7XXE=";
-            allowedIPs = [ "172.28.0.0/16" ];
+            allowedIPs = [ "172.28.0.0/16" "192.168.44.0/24" ];
             endpoint = "duck.srv.fsn.lama-corp.space:51820";
             persistentKeepalive = 25;
           }
