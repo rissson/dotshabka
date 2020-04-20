98fac598a392c880416c726bbeb7b9cc1b584b1e
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2020-04-21 00:22:54 +0200

    duck: monitoring: add kibana

--- hosts/duck/monitoring/elk/default.nix
+++ hosts/duck/monitoring/elk/default.nix
@@ -3,10 +3,17 @@
 {
   services.elasticsearch = {
     enable = true;
-    dataDir = "/srv/elasticsearch";
+    dataDir = "/srv/elk/elasticsearch";
+    cluster_name = "lama-corp";
     extraConf = ''
       node.name: "duck"
       node.master: true
     '';
+  };
+
+  services.kibana = {
+    enable = true;
+    dataDir = "/srv/elk/kibana";
+    listenAddress = "172.28.1.1";
   };
 }
