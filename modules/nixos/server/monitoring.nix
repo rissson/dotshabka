aee7b4e556e739d6e4224fb3537531e588a3dc8a
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2020-04-20 18:21:29 +0200

    duck: monitoring: disable fping

--- modules/nixos/server/monitoring.nix
+++ modules/nixos/server/monitoring.nix
@@ -12,19 +12,4 @@ in {
   ];
 
   services.netdata.enable = true;
-
-  environment.etc = mkIf config.services.netdata.enable {
-    "netdata/fping.conf".text = ''
-      fping="${pkgs.fping}/bin/fping"
-      hosts="duck.srv.fsn.lama-corp.space hub.virt.duck.srv.fsn.lama-corp.space nas.srv.bar.lama-corp.space"
-    '';
-  };
-
-  security.wrappers."fping.plugin" = mkIf config.services.netdata.enable {
-    source = "${pkgs.netdata}/libexec/netdata/plugins.d/fping.plugin";
-    capabilities = "cap_net_raw+ep";
-    owner = config.services.netdata.user;
-    group = config.services.netdata.group;
-    permissions = "u+rx,g+rx,o-rwx";
-  };
 }
