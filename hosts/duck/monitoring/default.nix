349cb3cbbf2ebd2b6b26a865dfbe2cce3d7fd4d4
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2020-04-20 18:52:49 +0200

    duck: monitoring: enable httpcheck

--- hosts/duck/monitoring/default.nix
+++ hosts/duck/monitoring/default.nix
@@ -6,25 +6,20 @@ with lib;
   environment.etc = mkIf config.services.netdata.enable {
     "netdata/go.d.conf".text = ''
       modules:
-        apache: no
-        bind: no
-        example: no
-        freeradius: no
+        httpcheck: yes
         lighttpd: no
-        mysql: no
         nginx: yes
-        openvpn: no
         phpfpm: no
-        rabbitmq: no
-        squidlog: no
         web_log: yes
         whoisquery: no
     '';
+    "netdata/go.d/httpcheck.conf".text = builtins.readFile ./go.d/httpcheck.conf;
     "netdata/go.d/nginx.conf".text = builtins.readFile ./go.d/nginx.conf;
     "netdata/go.d/web_log.conf".text = builtins.readFile ./go.d/web_log.conf;
 
     "netdata/python.d.conf".text = ''
       example: no
+      httpcheck: no
       logind: yes
       nginx: no
       web_log: no
