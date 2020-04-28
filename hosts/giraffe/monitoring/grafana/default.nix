07ad8239007cc12763ecf87c0e6e9ff59e9575bf
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2020-04-29 00:13:15 +0200

    giraffe: grafana: provision dashboards and datasources

--- hosts/giraffe/monitoring/grafana/default.nix
+++ hosts/giraffe/monitoring/grafana/default.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ lib, ... }:
 
 {
   services.grafana = {
@@ -6,5 +6,39 @@
     addr = "172.28.3.1";
     dataDir = "/srv/grafana";
     rootUrl = "http://172.28.3.1:3000";
+
+    provision = {
+      enable = true;
+      dashboards = [
+        {
+          folder = "Netdata per-host";
+          options.path = "/etc/grafana/dashboards/netdata-per-host";
+          disableDeletion = true;
+          updateIntervalSeconds = 10;
+        }
+        {
+          folder = "Web";
+          options.path = "/etc/grafana/dashboards/web";
+          disableDeletion = true;
+          updateIntervalSeconds = 10;
+        }
+      ];
+      datasources = [
+        {
+          name = "InfluxDB";
+          type = "influxdb";
+          url = "http://localhost:8086";
+          isDefault = true;
+          editable = false;
+          access = "proxy";
+          database = "opentsdb_netdata";
+        }
+      ];
+    };
+  };
+
+  environment.etc = {
+    "grafana/dashboards/netdata-per-host".source = ./dashboards/netdata-per-host;
+    "grafana/dashboards/web".source = ./dashboards/web;
   };
 }
