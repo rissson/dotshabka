b8bd3c5e1203ef626827653435639312b5a2a434
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2020-04-30 21:38:48 +0200

    duck: dns: add mail-1

--- hosts/duck/networking/dns.nix
+++ hosts/duck/networking/dns.nix
@@ -60,9 +60,10 @@ in with dotshabka.data.space.lama-corp; {
         local-zone: "fsn.lama-corp.space." static
         local-data: "duck.srv.fsn.lama-corp.space. IN A ${fsn.srv.duck.wg.v4.ip}"
         local-data-ptr: "${fsn.srv.duck.wg.v4.ip} duck.srv.fsn.lama-corp.space"
-        local-data: "smtp-1.duck.srv.fsn.lama-corp.space IN CNAME duck.srv.fsn.lama-corp.space"
         local-data: "hub.virt.duck.srv.fsn.lama-corp.space. IN A ${fsn.srv.duck.virt.hub.wg.v4.ip}"
         local-data-ptr: "${fsn.srv.duck.virt.hub.wg.v4.ip} hub.virt.duck.srv.fsn.lama-corp.space"
+        local-data: "mail-1.duck.srv.fsn.lama-corp.space. IN A ${fsn.srv.duck.mail-1.internal.v4.ip}"
+        local-data-ptr: "${fsn.srv.duck.mail-1.internal.v4.ip} mail-1.duck.srv.fsn.lama-corp.space"
 
         domain-insecure: "nbg.lama-corp.space"
         local-zone: "nbg.lama-corp.space." static
