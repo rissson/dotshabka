c15aacfadb6506a647a5962aea8ac4b94bd306f6
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2020-05-05 21:52:53 +0200

    minio-1: add minio

--- hosts/minio-1/minio/default.nix
+++ hosts/minio-1/minio/default.nix
@@ -1,4 +1,11 @@
 { ... }:
 
-{
+let port = 19000;
+in {
+  services.minio = {
+    enable = false; # Enabled by secrets
+    listenAddress = ":${toString port}";
+    configDir = "/srv/minio/config";
+    dataDir = "/srv/minio/data";
+  };
 }
