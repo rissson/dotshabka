22842a811d690d99ea72981c27a574ee590e6b4b
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2020-05-07 02:25:55 +0200

    nginx hotfix

--- hosts/minio-1/minio/default.nix
+++ hosts/minio-1/minio/default.nix
@@ -2,12 +2,25 @@
 
 let port = 19000;
 in {
-  networking.firewall.allowedTCPPorts = [ port ];
+  networking.firewall.allowedTCPPorts = [ port 19001 ];
 
   services.minio = {
     enable = false; # Enabled by secrets
     listenAddress = ":${toString port}";
     configDir = "/srv/minio/config";
     dataDir = "/srv/minio/data";
+  };
+
+  services.nginx = {
+    enable = true;
+    virtualHosts."tp14.acdc.risson.space.minio-1.vrt.fsn.lama-corp.space" = {
+      listen = [
+        { addr = "0.0.0.0"; port = 19001; }
+        { addr = "[::]"; port = 19001; }
+      ];
+      locations."/" = {
+        proxyPass = "http://localhost:19000/tp14.acdc.risson.space$is_args$args";
+      };
+    };
   };
 }
