4a80e4572e213a69839939804248b5da7bd19533
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2020-05-07 09:48:35 +0200

    acdc-tp14-1: docker-compose: add minio

--- hosts/minio-1/minio/default.nix
+++ hosts/minio-1/minio/default.nix
@@ -2,25 +2,12 @@
 
 let port = 19000;
 in {
-  networking.firewall.allowedTCPPorts = [ port 19001 ];
+  networking.firewall.allowedTCPPorts = [ port ];
 
   services.minio = {
     enable = false; # Enabled by secrets
     listenAddress = ":${toString port}";
     configDir = "/srv/minio/config";
     dataDir = "/srv/minio/data";
-  };
-
-  services.nginx = {
-    enable = true;
-    virtualHosts."tp14.acdc.risson.space.minio-1.vrt.fsn.lama-corp.space" = {
-      listen = [
-        { addr = "0.0.0.0"; port = 19001; }
-        { addr = "[::]"; port = 19001; }
-      ];
-      locations."/" = {
-        proxyPass = "http://localhost:19000/tp14.acdc.risson.space$is_args$args";
-      };
-    };
   };
 }
