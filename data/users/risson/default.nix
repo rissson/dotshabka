14aa46db68b46b4184116df8bb1e48038bfb5ffd
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2020-06-19 17:09:28 +0200

    data: users: risson: change password

--- data/users/risson/default.nix
+++ data/users/risson/default.nix
@@ -3,12 +3,11 @@
 {
   uid = 2000;
   hashedPassword =
-    "$6$2YnxY3Tl$kRj7YZypnB2Od41GgpwYRcn4kCcCE6OksZlKLws0rEi//T/emKWEsUZZ2ZG40eph1bpmjznztav4iKc8scmqc1";
+    "$6$41nyy7f5EIwor$RRn88LX1mgJeqbkAwHWs1cIew7mlJ8lDASKlDYX4OmXgIB7y53CllTPx1KIJ4x/utx9UYBOdzAFkU9zvi2sOw1";
   sshKeys = [
     "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC9Fz7AgVw7E8sgg6qXWJxXjfyvmhqYhkXcAoP7YfC+nJ9MLMvgqztSEmBuNLrq3M7FYGgTxNUldGF38CleihvKTNCU/MiHptGd0lctc57Xcxp3ij2ZH9j90vE/DKjEeBaLCfuVmo5AkBfVpC0t8a6Dg83NUp9B7Cf/iCkd274MF3ccd3L5pqdAZucx6frKqmcyngW8y3ytxJEG3sL4e1Oypc+yYIlpOiFliOexxbToOFkDMNIEvJjvjPFY2K025K7FEG8iVoGD1Y4W+dgi5FKhE9Glx3W08CWlw31TLZuFwkNZS8g1yq0fcJfkmPlLElOIWt4sQcW3dFMY+yUozHQqcy3FeNh98+mcrnQUpwjcM2bKbmfa+HDNcFx+guZPgI84dycvFrmiyOjhnqQDqdmST3FxYVReupGSO4c4XiAfQBTqnpEWTxrs8XppcH4kdx37v9tlu20HtiXtc2C/3l65D6oUerbjnNOzUsLjtbg9CwQuWZ73nqI7izZUbmb2oqvXQqtrp8W7CSTT501FmU5mZqaqW9mn7QyexEyaurPxUnBqB2XaO1UkRs4eM8D2D4ZCLDvKCfCfJ2c5/0MbPkgjvlp2HBXr5pzVYE/ZRIcNAIezA2izlxwAMmmIPVyqjSFsjXnDbW+Buuh0kjKk95z0WkpYgGk/q9/Qe8nXh/Qd4w== yubiyubi"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGB3YtCoN8hXIHDq80mijkLfNy5elUueN7LIy61Jex2/ yubicri"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKQ5Ob4xxSs+DgQXTUft9Fc8tIcga3rMOrq7dg3OxwdS risson@duck"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKRZBjJWWFcvXNtBPWx1qOTdGdIVxEMAzv3lTjBWLvXF root@duck"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDNdzD8Ue7Agm/mIk13oCeJuC1muVC3KajT1ruOLp4m2 risson@goat"
   ];
 }
