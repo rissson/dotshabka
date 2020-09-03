35a9ab15b700fb94746fa91d07b138c58eb56a71
Marc 'risson' Schmitt <marc.schmitt@lama-corp.space>
2020-09-03 13:53:27 +0200

    kvm-1: disable docker
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@lama-corp.space>

--- hosts/kvm-1/configuration.nix
+++ hosts/kvm-1/configuration.nix
@@ -28,8 +28,6 @@ with lib;
 
   nix.gc.automatic = mkForce false;
 
-  shabka.virtualisation.docker.enable = true;
-
   # This value determines the NixOS release with which your system is to be
   # compatible, in order to avoid breaking some software such as database
   # servers. You should change this only after NixOS release notes say you
