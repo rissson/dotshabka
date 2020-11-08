87e7c69f2c014554c5c04cd3eada9c953275c446
Marc 'risson' Schmitt <marc.schmitt@lama-corp.space>
2020-11-08 22:43:59 +0100

    profiles: k8s: add certs and master/etcd
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@lama-corp.space>

--- profiles/k8s/master/default.nix
+++ profiles/k8s/master/default.nix
@@ -1,5 +1,12 @@
-{ ... }:
+{ config, pkgs, lib, ... }:
 
+let
+  nodeIP = (lib.head config.networking.interfaces.ens3.ipv4.addresses).address;
+in
 {
+  imports = [
+    ./etcd.nix
+  ];
 
+  sops.validateSopsFiles = false;
 }
