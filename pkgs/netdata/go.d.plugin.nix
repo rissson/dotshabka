38f6bb2889b881d6d99ce2e8680b3202ee7820a5
Marc 'risson' Schmitt <marc.schmitt@lama-corp.space>
2020-11-19 09:41:19 +0100

    pkgs: netdata: go.d.plugin: 0.20.0 -> 0.25.0
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@lama-corp.space>

--- pkgs/netdata/go.d.plugin.nix
+++ pkgs/netdata/go.d.plugin.nix
@@ -2,16 +2,16 @@
 
 buildGoModule rec {
   pname = "netdata-go.d.plugin";
-  version = "0.20.0";
+  version = "0.25.0";
 
   src = fetchFromGitHub {
     owner = "netdata";
     repo = "go.d.plugin";
     rev = "v${version}";
-    sha256 = "0wd1wg56q955jm5ksq2zqzlms1nlxx7n7vv43l096k1578fv93jv";
+    sha256 = "0cp1asw10a8ndndzq8r57mncrm8521aw3x081vrlfqvhp6qahr3j";
   };
 
-  vendorSha256 = "1k84l97fw4s9jdwbka4p168m7l7wil0c4cpijis8ypj3g1xfrw90";
+  vendorSha256 = "sha256-uei4Wt0B9Bm/V5ykZ2KP9rwMjmpxy4dFEkeieVmKZpk=";
 
   doCheck = false;
 
