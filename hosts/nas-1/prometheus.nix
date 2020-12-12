a81fd969f603c90ac2ed8b05119a6f27260f20f4
Marc 'risson' Schmitt <marc.schmitt@lama-corp.space>
2020-12-12 11:43:59 +0100

    nas-1: prometheus: scrape netdata
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@lama-corp.space>

--- hosts/nas-1/prometheus.nix
+++ hosts/nas-1/prometheus.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ config, ... }:
 
 {
   services.prometheus = {
@@ -12,6 +12,34 @@
         scrape_interval = "30s";
         static_configs = [
           { targets = [ "cuckoo.srv.bar.lama-corp.space:80" ]; }
+        ];
+      }
+      {
+        job_name = "netdata-scrape";
+        scrape_interval = "15s";
+        metrics_path = "/api/v1/allmetrics";
+        params = {
+          format = [ "prometheus_all_hosts" ];
+          source = [ "average" ];
+          server = [ config.networking.fqdn ];
+        };
+        honor_labels = true;
+        static_configs = [
+          {
+            targets = map (host: "${host}.lama-corp.space:19999") [
+              "cuckoo.srv.bar"
+              "nas-1.srv.bar"
+
+              "kvm-2.srv.fsn"
+              "mail-1.vrt.fsn"
+              "master-11.k8s.fsn"
+              "master-12.k8s.fsn"
+              "master-13.k8s.fsn"
+              "worker-11.k8s.fsn"
+              "worker-12.k8s.fsn"
+              "worker-13.k8s.fsn"
+            ];
+          }
         ];
       }
     ];
