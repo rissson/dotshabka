9c2ffdd73ac00fd43889dc019c779e02e4de910c
Marc 'risson' Schmitt <marc.schmitt@lama-corp.space>
2021-01-13 00:16:10 +0100

    fsn/kvm-2: nginx: also listen on internal network
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@lama-corp.space>

--- hosts/fsn/kvm-2/nginx.nix
+++ hosts/fsn/kvm-2/nginx.nix
@@ -38,6 +38,8 @@
       listen = [
         { addr = "168.119.71.47"; port = 80; }
         { addr = "168.119.71.47"; port = 443; ssl = true; }
+        { addr = "172.28.254.6"; port = 80; }
+        { addr = "172.28.254.6"; port = 443; ssl = true; }
       ];
       forceSSL = true;
       enableACME = true;
@@ -48,6 +50,8 @@
       listen = [
         { addr = "168.119.71.47"; port = 80; }
         { addr = "168.119.71.47"; port = 443; ssl = true; }
+        { addr = "172.28.254.6"; port = 80; }
+        { addr = "172.28.254.6"; port = 443; ssl = true; }
       ];
       forceSSL = true;
       enableACME = true;
