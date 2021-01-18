ee6b5f99458344353cd888c8278d1486c101bfaf
Marc 'risson' Schmitt <marc.schmitt@lama-corp.space>
2021-01-18 11:01:40 +0100

    fsn/kvm-2: networking: remove from peering wireguard
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@lama-corp.space>

--- hosts/fsn/kvm-2/networking/wireguard.nix
+++ hosts/fsn/kvm-2/networking/wireguard.nix
@@ -2,7 +2,6 @@
 {
   networking.firewall.allowedUDPPorts = [
     config.networking.wireguard.interfaces.wg0.listenPort
-    config.networking.wireguard.interfaces.wg212270.listenPort
   ];
 
   networking = {
@@ -81,52 +80,9 @@
             }
           ];
         };
-
-        wg212270 = {
-          ips = [
-            "fd3c:c1c4:bbff:9a64::dead:beef/64"
-          ];
-          listenPort = 51821;
-          privateKeyFile = config.sops.secrets.wireguard_wg212270_private_key.path;
-
-          allowedIPsAsRoutes = false;
-          peers = [
-            {
-              # edge-1.srv.par.lama-corp.space
-              publicKey = "4W30sjYQxN8FpGAIl8eEFzGQLfeQvoQZmg3kBwe1J1s=";
-              allowedIPs = [
-                "fd3c:c1c4:bbff:9a64::ee1/128"
-              ];
-              endpoint = "[2a05:f480:1c00:9ee::1]:51821";
-              persistentKeepalive = 60;
-            }
-            {
-              # tonkinois.core.vlt.phowork.fr
-              publicKey = "E1EBL3208LQ6xt3NmxSQdkgmrPMH6bjO0tTDdASJPR8=";
-              allowedIPs = [
-                "fd3c:c1c4:bbff:9a64::cafe/128"
-                "2a0f:9240:2000::/48"
-                "2a0e:b107:f50::/44"
-              ];
-              endpoint = "[2a05:f480:1c00:4c8:5400:3ff:fe1f:ae47]:51823";
-              persistentKeepalive = 60;
-            }
-            {
-              # colibri.deliciousmuffins.net
-              publicKey = "GHq5ONIpnsgdrBar2fvIgWCfPWK5wKi4LUmF6MBjIjU=";
-              allowedIPs = [
-                "fd3c:c1c4:bbff:9a64::4251/128"
-                "2001:db8:4251::/48"
-              ];
-              endpoint = "[2a05:f480:1c00:e18:5400:3ff:fe21:317a]:51820";
-              persistentKeepalive = 60;
-            }
-          ];
-        };
       };
     };
   };
 
   sops.secrets.wireguard_wg0_private_key.sopsFile = ./wireguard.yml;
-  sops.secrets.wireguard_wg212270_private_key.sopsFile = ./wireguard.yml;
 }
