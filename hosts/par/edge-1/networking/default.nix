4e4574e1250e5ea5bfe9b728bc91f4e30ff310e2
Marc 'risson' Schmitt <marc.schmitt@lama-corp.space>
2021-01-18 22:57:59 +0100

    par/edge-1: networking: use DHCP
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@lama-corp.space>

--- hosts/par/edge-1/networking/default.nix
+++ hosts/par/edge-1/networking/default.nix
@@ -21,24 +21,6 @@
     nameservers = [ "127.0.0.1" "::1" ];
 
     useDHCP = false;
-
-    interfaces = {
-      ens3 = {
-        useDHCP = true;
-        ipv4.addresses = [{
-          address = "108.61.208.236";
-          prefixLength = 23;
-        }];
-        ipv6.addresses = [{
-          address = "2a05:f480:1c00:9ee::1";
-          prefixLength = 128;
-        }];
-      };
-    };
-
-    defaultGateway = {
-      address = "108.61.208.1";
-      interface = "ens3";
-    };
+    interfaces.ens3.useDHCP = true;
   };
 }
