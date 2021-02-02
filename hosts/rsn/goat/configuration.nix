be7996c476255542b0cb0ae07f03456113eb9b8c
Marc 'risson' Schmitt <marc.schmitt@lama-corp.space>
2021-02-02 12:43:07 +0100

    rsn/goat: xserver: add nouveau driver
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@lama-corp.space>

--- hosts/rsn/goat/configuration.nix
+++ hosts/rsn/goat/configuration.nix
@@ -95,6 +95,8 @@
     127.0.0.1 code.cri.epita.net
   '';
 
+  services.xserver.videoDrivers = [ "nouveau" ];
+
   # This value determines the NixOS release with which your system is to be
   # compatible, in order to avoid breaking some software such as database
   # servers. You should change this only after NixOS release notes say you
