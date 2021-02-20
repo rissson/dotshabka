76af73764c7d2629bcd627a7318f7e0490bf88d3
Marc 'risson' Schmitt <marc.schmitt@lama-corp.space>
2021-02-20 17:48:17 +0100

    fsn/kvm-2: fix internal DNS
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@lama-corp.space>

--- hosts/fsn/kvm-2/networking/dns.nix
+++ hosts/fsn/kvm-2/networking/dns.nix
@@ -6,6 +6,7 @@
     extraData = {
       "mail-1.vrt.fsn" = "172.28.6.11";
       "pine.vrt.fsn" = "172.28.6.201";
+      "kvm-2.srv.fsn" = "172.28.6.254";
 
       "master-11.k8s.fsn" = "172.28.7.11";
       "master-12.k8s.fsn" = "172.28.7.12";
@@ -13,6 +14,7 @@
       "worker-11.k8s.fsn" = "172.28.7.111";
       "worker-12.k8s.fsn" = "172.28.7.112";
       "worker-13.k8s.fsn" = "172.28.7.113";
+      "kvm-2.k8s.fsn" = "172.28.7.254";
 
       # k8s
       "ingress.k8s.fsn" = "172.28.8.10";
