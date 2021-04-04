d91795c858c56d032b471a25e731727e4c759989
Marc 'risson' Schmitt <marc.schmitt@lama-corp.space>
2021-04-04 13:39:51 +0200

    rsn/hedgehog: networking: remove unused localCommands
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@lama-corp.space>

--- hosts/rsn/hedgehog/networking/default.nix
+++ hosts/rsn/hedgehog/networking/default.nix
@@ -23,38 +23,10 @@
       interfaces = [ "wlp1s0" ];
     };
 
-    localCommands = let ethtool = "${pkgs.ethtool}/bin/ethtool"; in ''
-      ip link del myfw-in-nsin
-      ip link del myfw-in-nsfw
-      ip link del myfw-out-nsout
-      ip link del myfw-out-nsfw
-
-      ip netns delete nsin
-      ip netns delete nsfw
-      ip netns delete nsout
-
-      ip netns add nsin
-      ip netns add nsfw
-      ip netns add nsout
-
-      ip link add myfw-in-nsin type veth peer name myfw-in-nsfw
-      ip link set dev myfw-in-nsin netns nsin up
-      ip link set dev myfw-in-nsfw netns nsfw up
-
-      ip netns exec nsin ${ethtool} -K myfw-in-nsin tx off
-      ip netns exec nsfw ${ethtool} -K myfw-in-nsfw tx off
-
-      ip netns exec nsin ip addr add fd00::0/127 dev myfw-in-nsin
-
-      ip link add myfw-out-nsout type veth peer name myfw-out-nsfw
-      ip link set dev myfw-out-nsout netns nsout up
-      ip link set dev myfw-out-nsfw netns nsfw up
-
-      ip netns exec nsout ${ethtool} -K myfw-out-nsout tx off
-      ip netns exec nsfw ${ethtool} -K myfw-out-nsfw tx off
-
-      ip netns exec nsout ip addr add fd00::1/127 dev myfw-out-nsout
-    '';
+    firewall = {
+      allowedTCPPorts = [ 5060 5061 ];
+      allowedUDPPorts = [ 5060 5061 7078 7079 9078 ];
+    };
   };
 
   sops.secrets.wpa_supplicant = {
