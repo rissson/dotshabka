2c5204baef21f46c40190495da7c187a0c9116d1
Marc 'risson' Schmitt <marc.schmitt@lama-corp.space>
2021-08-28 16:19:26 +0200

    profiles/workstation: home/risson: tmuxp: update cri
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@lama-corp.space>

--- profiles/workstation/home/risson/tmuxp/cri.nix
+++ profiles/workstation/home/risson/tmuxp/cri.nix
@@ -3,28 +3,48 @@
   start_directory = "~/cri";
   windows = [
     {
-      start_directory = "infra/ansible";
-      window_name = "ansible";
-    }
-    {
-      start_directory = "iac/undercloud";
+      start_directory = "iac/infrastructure/undercloud";
       window_name = "under 1";
     }
     {
-      start_directory = "iac/undercloud";
+      start_directory = "iac/infrastructure/undercloud";
       window_name = "under 2";
     }
     {
-      start_directory = "iac/overcloud";
+      start_directory = "iac/infrastructure/kolla";
+      window_name = "kolla";
+    }
+    {
+      start_directory = "iac/infrastructure/overcloud";
       window_name = "over 1";
     }
     {
-      start_directory = "iac/overcloud";
+      start_directory = "iac/infrastructure/overcloud/terraform";
       window_name = "over 2";
     }
     {
+      start_directory = "iac/infrastructure/k8s/argocd";
+      window_name = "argocd";
+    }
+    {
+      start_directory = "iac/infrastructure/k8s/clusters/apps";
+      window_name = "cluster apps";
+    }
+    {
+      start_directory = "iac/infrastructure/k8s/clusters/others";
+      window_name = "cluster others";
+    }
+    {
+      start_directory = "iac/infrastructure/k8s/apps";
+      window_name = "apps";
+    }
+    {
+      start_directory = "iac/infrastructure/k8s/secrets";
+      window_name = "secrets";
+    }
+    {
       start_directory = "packages/kolla-ansible";
-      window_name = "kolla";
+      window_name = "kolla-ansible";
     }
   ];
 }
