ee0fd63d212a2c6366a6aaf01ea6e96d05ceda69
Marc 'risson' Schmitt <marc.schmitt@lama-corp.space>
2022-02-10 20:29:14 +0100

    bar/nas-1: fix backups
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@lama-corp.space>

--- hosts/bar/nas-1/backups/default.nix
+++ hosts/bar/nas-1/backups/default.nix
@@ -5,27 +5,29 @@
     enable = true;
     interval = "hourly";
     sshKey = config.sops.secrets.syncoid_ssh_key.path;
-    user = "root";
 
     commands = {
       "root@kvm-2.srv.fsn.lama-corp.space:rpool/persist" = {
-        target = "rpool/backups/kvm-2.srv.fsn/persist";
+        target = "rpool/backups/kvm-2.fsn.lama.tel/persist";
         recursive = true;
       };
       "root@kvm-2.srv.fsn.lama-corp.space:rpool/vms/k3s-1" = {
-        target = "rpool/backups/kvm-2.srv.fsn/vms/k3s-1";
+        target = "rpool/backups/kvm-2.fsn.lama.tel/vms/k3s-1";
         recursive = true;
       };
       "root@kvm-2.srv.fsn.lama-corp.space:rpool/vms/mail-1" = {
-        target = "rpool/backups/kvm-2.srv.fsn/vms/mail-1";
+        target = "rpool/backups/kvm-2.fsn.lama.tel/vms/mail-1";
         recursive = true;
       };
       "root@kvm-2.srv.fsn.lama-corp.space:rpool/vms/pine" = {
-        target = "rpool/backups/kvm-2.srv.fsn/vms/pine";
+        target = "rpool/backups/kvm-2.fsn.lama.tel/vms/pine";
         recursive = true;
       };
     };
   };
 
-  sops.secrets.syncoid_ssh_key.sopsFile = ./syncoid_ssh_key.yml;
+  sops.secrets.syncoid_ssh_key = {
+    sopsFile = ./syncoid_ssh_key.yml;
+    owner = config.services.syncoid.user;
+  };
 }
