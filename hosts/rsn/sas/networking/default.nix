e9bb136c86086fd860f2e3f96d9a7f69b389a384
Marc 'risson' Schmitt <marc.schmitt@lama-corp.space>
2022-02-24 09:41:21 +0100

    rsn/sas: add smart hosts
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@lama-corp.space>

--- hosts/rsn/sas/networking/default.nix
+++ hosts/rsn/sas/networking/default.nix
@@ -1,10 +1,41 @@
 { soxincfg, config, pkgs, lib, ... }:
 
+let
+  smartHosts = pkgs.stdenv.mkDerivation {
+    name = "smart-hosts";
+
+    src = pkgs.fetchurl {
+      url = "http://infra.smartadserver.com/get-hosts.asp";
+      sha256 = "sha256-qnyCsVQJKEzI317imiwl5Sgh/tGVZhhnAYfLwBOlcxc=";
+    };
+
+    phases = [ "buildPhase" ];
+
+    buildInputs = [ pkgs.dos2unix ];
+
+    buildPhase = ''
+      dos2unix --newfile $src tmp
+
+      # Remove n-1 lines from the beginning of the file
+      tail -n +4 tmp > tmp2
+
+
+      # Remove n-1 lines from the end of the file
+      head -n -3 tmp2 > $out
+    '';
+  };
+in
 {
   networking = {
     hostName = "sas";
     domain = "rsn.lama.tel";
     hostId = "8425e349";
+
+    extraHosts = ''
+      10.3.10.18 infra.smartadserver.com
+    '';
+
+    hostFiles = [ smartHosts ];
 
     nameservers = [ "1.1.1.1" ];
 
