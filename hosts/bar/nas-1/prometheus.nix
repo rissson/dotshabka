5cd48bd2110171a2f540fed0cb4a33a9b41faccb
Marc 'risson' Schmitt <marc.schmitt@lama-corp.space>
2022-03-14 15:29:21 +0100

    bar/nas-1: prometheus: remove netdata
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@lama-corp.space>

--- hosts/bar/nas-1/prometheus.nix
+++ hosts/bar/nas-1/prometheus.nix
@@ -14,34 +14,6 @@
           { targets = [ "cuckoo.srv.bar.lama-corp.space:80" ]; }
         ];
       }
-      {
-        job_name = "netdata-scrape";
-        scrape_interval = "15s";
-        metrics_path = "/api/v1/allmetrics";
-        params = {
-          format = [ "prometheus_all_hosts" ];
-          source = [ "average" ];
-          server = [ config.networking.fqdn ];
-        };
-        honor_labels = true;
-        static_configs = [
-          {
-            targets = map (host: "${host}.lama-corp.space:19999") [
-              "cuckoo.srv.bar"
-              "nas-1.srv.bar"
-
-              "kvm-2.srv.fsn"
-              "mail-1.vrt.fsn"
-              "master-11.k8s.fsn"
-              "master-12.k8s.fsn"
-              "master-13.k8s.fsn"
-              "worker-11.k8s.fsn"
-              "worker-12.k8s.fsn"
-              "worker-13.k8s.fsn"
-            ];
-          }
-        ];
-      }
     ];
   };
 }
