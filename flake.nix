0a2a36beed5d6e0aeb4b39fdc98b33302ed52f83
Marc 'risson' Schmitt <marc.schmitt@lama-corp.space>
2022-06-27 00:03:49 +0200

    flake: update inputs
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@lama-corp.space>

--- flake.nix
+++ flake.nix
@@ -4,11 +4,11 @@
   description = "Lama Corp. infrastructure configurations.";
 
   inputs = rec {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nixpkgs-master.url = "github:NixOS/nixpkgs/master";
     home-manager = {
-      url = "github:nix-community/home-manager/release-21.11";
+      url = "github:nix-community/home-manager/release-22.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
